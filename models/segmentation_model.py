a44e82d750d16f91226be926c7460b851c336c51
IgorStetsenko <i.a.stetsenko@gmail.com>
2025-01-18 21:20:07 +0300

    Добавил базовый пайплайн формирования координат органов для ЭИТ

--- models/segmentation_model.py
+++ models/segmentation_model.py
@@ -1,1 +1,16 @@
 # Model for tomographic image segmentation
+
+def run_ribs_segmentation():
+    pass
+
+
+def run_slice_segmentation():
+    pass
+
+
+def create_abs_coordinate_file():
+    pass
+
+
+if __name__ == "__main__":
+    create_abs_coordinate_file()
