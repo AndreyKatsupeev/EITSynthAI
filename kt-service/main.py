a7fe861c3b513f9a673a87b342652315b79d7aca
IgorStetsenko <i.a.stetsenko@gmail.com>
2025-02-16 20:56:40 +0300

    Добавил первичный набросок функционала

--- kt-service/main.py
+++ kt-service/main.py
@@ -1,9 +1,41 @@
-[main_settings]
-    service_version = '1.0'
-    save_log_path = ['ai_logs']
+from ai_tools.tools import image_decoder
+from ai_tools.fsi_logger import write_log
+
+from fastapi import FastAPI, UploadFile, File, Request
+from typing import List, Optional
+
+app = FastAPI()
 
 
-[ai_settings]
-    device = "cuda"
-    weights_ribs = '/'
-    weights_segmentation = '/'
+@app.post('/uploadDicom')
+def dicom_handler(files: List[UploadFile] = File(...)):
+    """
+
+    Args:
+        files:
+
+    Returns:
+
+    """
+
+    list_image = image_decoder(files)
+    number_out, max_proba_area, max_proba_image_index, error_message = numberRecognition(). \
+        digits_detector(list_image, device, model_yolo_area, model_yolo_num)  # run neural networks
+    write_log("out_digits_detector", [number_out, max_proba_area, max_proba_image_index, error_message])
+
+    yolo_message = create_yolo_message(number_out, max_proba_area,
+                                       max_proba_image_index, error_message)  # create yolo message response
+    return yolo_message
+
+
+@app.post('/uploadImage')
+def image_handler(files: List[UploadFile] = File(...)):
+    """
+
+    Args:
+        files:
+
+    Returns:
+
+    """
+    pass
